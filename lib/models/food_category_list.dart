ec8e026fca167617a6bd3eec2008392575208525
allenjiji <allenjiji2604@gmail.com>
2020-08-23 03:27:49 +0530

    added food_category_screen

--- lib/models/food_category_list.dart
+++ lib/models/food_category_list.dart
@@ -3,77 +3,77 @@ class FoodCategoryList{
 
   List<dynamic> foodItems = [
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/icecream.png",
       "foodName": "Ice Cream",
       "index": 0
     },
     {
-      "image": "assets/img/salon.png",
-      "foodName": "Tea and Coffee",
+      "image": "assets/img/coffee.png",
+      "foodName": "Tea & Coffee",
       "index": 1
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/pizza.jpg",
       "foodName": "Pizza",
       "index": 2
     },
     {
-      "image": "assets/img/salon.png",
-      "foodName": "Fish and Chicken",
+      "image": "assets/img/chicken.jpg",
+      "foodName": "Fish & Chicken",
       "index": 3
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/meal.png",
       "foodName": "Meal",
       "index": 4
     },
     {
-      "image": "assets/img/salon.png",
-      "foodName": "Shakes and Juices",
+      "image": "assets/img/shakes.jpg",
+      "foodName": "Shakes & Juices",
       "index": 5
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/shakes.jpg",
       "foodName": "Soft Drinks",
       "index": 6
     },
     {
-      "image": "assets/img/salon.png",
-      "foodName": "Burgers and Sandwiches",
+      "image": "assets/img/burger.jpg",
+      "foodName": "Burgers &\nSandwiches",
       "index": 7
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/sweets.png",
       "foodName": "Sweets",
       "index": 8
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/breakfast.jpg",
       "foodName": "Breakfast",
       "index": 9
     },
     {
-      "image": "assets/img/salon.png",
-      "foodName": "Cakes and Pastries",
+      "image": "assets/img/cake.jpg",
+      "foodName": "Cakes & Pastries",
       "index": 10
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/cookie.jpg",
       "foodName": "Cookies",
       "index": 11
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/snack.png",
       "foodName": "Snacks",
       "index": 12
     },
     {
-      "image": "assets/img/salon.png",
-      "foodName": "Fruits and Vegetables",
+      "image": "assets/img/fruits.jpg",
+      "foodName": "Fruits &\nVegetables",
       "index": 13
     },
     {
-      "image": "assets/img/salon.png",
+      "image": "assets/img/liquor.png",
       "foodName": "Paan",
       "index": 14
     },
