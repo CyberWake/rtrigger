8f9203aa23d4f707756c9eb063b18374e6952ebc
Manik Garg <manikgarg2000@gmail.com>
2020-08-22 09:56:20 +0530

    Rohit's Work

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return AuthProvider(
         auth: Auth(),
-    child: MaterialApp(
-    theme: ThemeData(
-    primarySwatch: Colors.blue,
-    ),
-    home: RootPage(),
-    ),
+        child: MaterialApp(
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: RootPage(),
+        ));
   }
 }
