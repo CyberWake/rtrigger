6955b02e592d0b3ac2769c4e9ec0a7d3e7901a7f
allenjiji <allenjiji2604@gmail.com>
2020-08-23 14:03:20 +0530

    make payment button added

--- lib/screens/Food/food_cart.dart
+++ lib/screens/Food/food_cart.dart
@@ -31,45 +31,57 @@ class _FoodCartState extends State<FoodCart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Your Cart"),
-        backgroundColor: AppTheme.dark_grey,
-      ),
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-                flex: 9,
-                child: Container(
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      return CartItemCard(
-                        vendorName: cartItems[index]["vendor"],
-                        price: cartItems[index]["price"],
-                        foodTitle: cartItems[index]["name"],
-                        distance: "2 km",
-                        time: "10 min",
-                        image: "assets/img/salon.png",
-                        quantity: cartItems[index]["quantity"],
-                        productID: cartItems[index]["productID"],
-                        onTap: () async {
-                          Cart cart = Cart();
-                          var deleteResult = await cart.deleteFromCart(
-                              userID: "Harsh",
-                              productID: cartItems[index]["productID"]);
-                          if (deleteResult == true) {
-                            getCartData();
-                          }
-                        },
-                      );
-                    },
-                    itemCount: cartItems.length,
-                  ),
-                )),
-          ],
+        appBar: AppBar(
+          title: Text("Your Cart"),
+          backgroundColor: AppTheme.dark_grey,
         ),
-      ),
-    );
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                  flex: 9,
+                  child: Container(
+                    child: ListView.builder(
+                      itemBuilder: (context, index) {
+                        return CartItemCard(
+                          vendorName: cartItems[index]["vendor"],
+                          price: cartItems[index]["price"],
+                          foodTitle: cartItems[index]["name"],
+                          distance: "2 km",
+                          time: "10 min",
+                          image: "assets/img/salon.png",
+                          quantity: cartItems[index]["quantity"],
+                          productID: cartItems[index]["productID"],
+                          onTap: () async {
+                            Cart cart = Cart();
+                            var deleteResult = await cart.deleteFromCart(
+                                userID: "Harsh",
+                                productID: cartItems[index]["productID"]);
+                            if (deleteResult == true) {
+                              getCartData();
+                            }
+                          },
+                        );
+                      },
+                      itemCount: cartItems.length,
+                    ),
+                  )),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: AppTheme.dark_grey,
+          elevation: 5,
+          child: Container(
+            child: FloatingActionButton.extended(
+              splashColor: AppTheme.darkerText,
+              elevation: 5,
+              backgroundColor: AppTheme.dark_grey,
+              onPressed: () {},//Implement Route To Payment Here
+              label: Text("Make Payment".toUpperCase()),
+            ),
+          ),
+        ));
   }
 }
