9760a3f5a1a3799e95c0b9752ed86cf7653db2b0
CyberWake <ritikfbd@gmail.com>
2020-08-26 17:41:53 +0530

    fixed all merged conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
     return AuthProvider(
         auth: Auth(),
         child: MaterialApp(
+          debugShowCheckedModeBanner: false,
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
