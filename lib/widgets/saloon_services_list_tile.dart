ecd6f9fdcf1a5caf919f8c06cb0cc4c960c21716
Keshav <keshurathore99@gmail.com>
2020-08-26 20:32:46 +0530

    Date and Time added in Saloon Screen

--- lib/widgets/saloon_services_list_tile.dart
+++ lib/widgets/saloon_services_list_tile.dart
@@ -1,102 +1,27 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:user/auth/auth.dart';
+import 'package:user/models/categories_enum.dart';
 import 'package:user/models/profile.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
+import 'package:user/screens/saloon_confirm_screen.dart';
 
 class SaloonServiceListTile extends StatefulWidget {
   final String service;
   final price;
+  final String uid;
+  final String location;
+  final String vendorName;
+  final Cards category;
 
-  SaloonServiceListTile(this.service, this.price);
+  SaloonServiceListTile(this.service, this.price, this.uid, this.location,
+      this.vendorName, this.category);
 
   @override
   _SaloonServiceListTileState createState() => _SaloonServiceListTileState();
 }
 
 class _SaloonServiceListTileState extends State<SaloonServiceListTile> {
-  Auth auth = Auth();
-  UserProfile profile;
-  Razorpay _razorpay;
-  bool isLoading = true;
-
-  void _handlePaymentError(PaymentFailureResponse response) async {
-    return await showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-          title: Text('An Error occured!'),
-          content:
-          Text(response.code.toString() + ' - ' + response.message),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Okay'),
-              onPressed: () {
-                Navigator.of(_).pop();
-              },
-            ),
-          ],
-        ));
-  }
-
-  void _handleExternalWallet(ExternalWalletResponse response) {}
-
-  void _handlePaymentSuccess(PaymentSuccessResponse response) async {
-    return await showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-          title: Text('Payment Successful.'),
-          content: Text(response.paymentId),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Okay'),
-              onPressed: () {
-                Navigator.of(_).pop();
-              },
-            ),
-          ],
-        ));
-  }
-
-  @override
-  void initState() {
-    auth.getProfile().whenComplete(() {
-      profile = auth.profile;
-      setState(() {
-        isLoading = false;
-      });
-    });
-    super.initState();
-    _razorpay = Razorpay();
-    _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
-    _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
-    _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
-  }
-
-  Future<void> makePayment() async {
-    var options = {
-      'key': 'rzp_test_Fs6iRWL4ppk5ng',
-      'amount': widget.price * 100, //in paise so * 100
-      'name': 'Rtiggers',
-      'description':
-      'Order Payment for id - ' + profile.username + widget.price.toString(),
-      'prefill': {'contact': profile.phone.toString(), 'email': profile.email},
-      "method": {
-        "netbanking": true,
-        "card": true,
-        "wallet": false,
-        "upi": true,
-      },
-    };
-    try {
-      _razorpay.open(options);
-    } catch (e) {
-      debugPrint(e);
-    }
-  }
-  void dispose() {
-    super.dispose();
-    _razorpay.clear();
-  }
-
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -131,12 +56,32 @@ class _SaloonServiceListTileState extends State<SaloonServiceListTile> {
                 color: Color.fromRGBO(00, 44, 64, 1.0),
                 textColor: Colors.white,
                 height: screenHeight / 24,
-                onPressed: () {
-                  makePayment();
+                onPressed: () async {
+                  final date = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime.now());
+//                      lastDate: DateTime(DateTime.now().year,
+//                          DateTime.now().month + 1, DateTime.now().day));
+                  final time = await showTimePicker(
+                      context: context, initialTime: TimeOfDay.now());
+                  final finalDate = DateTime(
+                      date.year, date.month, date.day, time.hour, time.minute);
+                  if (time != null && date != null)
+                    Navigator.of(context).push(CupertinoPageRoute(
+                        builder: (context) => SaloonConfirmScreen(
+                            widget.service,
+                            widget.price,
+                            widget.uid,
+                            finalDate,
+                            widget.vendorName,
+                            widget.location,
+                            widget.category)));
                 },
                 child: Text('Book Now'),
               ),
-            )
+            ),
           ],
         ));
   }
